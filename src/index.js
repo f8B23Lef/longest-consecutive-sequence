0f918413945b7a3b0f1c04885ac85f384ba57693
f8B23Lef <mal7rish@yandex.ru>
2018-09-22 00:30:12 +0300

    longest-consecutive-sequence/implement function which returns length of the longest sequence in array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+   
+  if(array.length === 0) return 0;
+
+  var set = new Set();
+  for(var i = 0; i < array.length; i++) {
+    set.add(array[i]);
+  }
+  
+  var maxCount = 1;
+  for (var item of set) {
+	  if(!set.has(item - 1)) {
+      var count = 1;
+      while(set.has(item + 1)) {
+        count++;
+        item++;
+        if(count > maxCount){
+          maxCount = count;
+        }
+      }
+    }
+  }
+  return maxCount;
 }

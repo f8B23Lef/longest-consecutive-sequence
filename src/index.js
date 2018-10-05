0120f19bb69c82e15b4d531b2bb14ac373b08c96
f8B23Lef <mal7rish@yandex.ru>
2018-10-05 21:31:59 +0300

    longest-consecutive-sequence/rewrite to ES6 notation

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ module.exports = function longestConsecutiveLength(array) {
    
   if(array.length === 0) return 0;
 
-  var set = new Set();
-  for(var i = 0; i < array.length; i++) {
+  const set = new Set();
+
+  for(let i = 0; i < array.length; i++) {
     set.add(array[i]);
   }
   
-  var maxCount = 1;
-  for (var item of set) {
-	  if(!set.has(item - 1)) {
-      var count = 1;
+  let maxCount = 1;
+  for (let item of set) {
+    if(!set.has(item - 1)) {
+      let count = 1;
       while(set.has(item + 1)) {
         count++;
         item++;
-        if(count > maxCount){
+        if(count > maxCount) {
           maxCount = count;
         }
       }
